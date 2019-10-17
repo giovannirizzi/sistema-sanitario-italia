5a5fe11808245eb7ff2276bda62d7b906f6aea08
Omar Battan <omarbattan@gmail.com>
2019-10-17 14:22:05 +0200

    Send Email iniziale

--- SistemaSanitario/src/main/java/sistemasanitario/servlets/ForgotPasswordServlet.java
+++ SistemaSanitario/src/main/java/sistemasanitario/servlets/ForgotPasswordServlet.java
@@ -5,6 +5,7 @@ import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -19,6 +20,14 @@ import sistemasanitario.entities.ResetPasswordToken;
 import sistemasanitario.entities.User;
 import sistemasanitario.utils.PasswordUtil;
 import sistemasanitario.utils.TokenUtil;
+
+//import to send email
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet("/forgotpassword")
 public class ForgotPasswordServlet extends HttpServlet {
@@ -60,7 +69,39 @@ public class ForgotPasswordServlet extends HttpServlet {
             try {
 
                 //TODO INVIA TOKEN PER EMAIL
+                final String host = "smtp.gmail.com";
+                final String port = "465";
+                final String username = "omarbattan@gmail.com";
+                final String passwordAcc = "dbfcdokqbhuspuop";
+                Properties props = System.getProperties();
+                props.setProperty("mail.smtp.host", host);
+                props.setProperty("mail.smtp.port", port);
+                props.setProperty("mail.smtp.socketFactory.port", port);
+                props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                props.setProperty("mail.smtp.auth", "true");
+                props.setProperty("mail.smtp.starttls.enable", "true");
+                props.setProperty("mail.debug", "true");
+                
+                Session session = Session.getInstance(props, new Authenticator() {
 
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, passwordAcc);
+                }
+                });
+                Message msg = new MimeMessage(session);
+                try {
+                    msg.setFrom(new InternetAddress(username));
+                    msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("omarbattan@gmail.com", false));
+                    msg.setSubject("Reset Password");
+                    msg.setText("Clicca sul link per il reset della password: \n"+"http://localhost:8080/SistemaSanitario/resetpassword?token="+token.token);
+                    msg.setSentDate(new Date());
+                    Transport.send(msg);
+                } catch (MessagingException me) {
+                    //TODO: log the exception
+                    me.printStackTrace(System.err);
+                }
+                
                 LOGGER.log(Level.INFO, "Reset password token: {0}", token.token );
 
                 token.token = TokenUtil.getHashSHA256(token.token);
@@ -69,13 +110,14 @@ public class ForgotPasswordServlet extends HttpServlet {
 
             } catch (SQLException ex) {
                 Logger.getLogger(ForgotPasswordServlet.class.getName()).log(Level.SEVERE, null, ex);
-            } 
+            }
         }
         request.setAttribute("email", email);
         RequestDispatcher dispatcher = getServletContext().
         getRequestDispatcher("/WEB-INF/forgotPassword.jsp");
         dispatcher.forward(request, response);     
     }
+    
     
     private User getUserByEmail(String email){
         
