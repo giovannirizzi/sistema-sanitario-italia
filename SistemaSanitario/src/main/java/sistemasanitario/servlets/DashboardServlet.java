8bdfcf2ce69133a1e7e56aede6ddcb3996224919
Marco <mark_mena97@hotmail.it>
2019-11-29 15:49:10 +0100

    Non ricordo bene cosa avevo fatto ahahah

--- SistemaSanitario/src/main/java/sistemasanitario/servlets/DashboardServlet.java
+++ SistemaSanitario/src/main/java/sistemasanitario/servlets/DashboardServlet.java
@@ -5,19 +5,44 @@
  */
 package sistemasanitario.servlets;
 
+import com.google.gson.Gson;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import sistemasanitario.entities.Medico;
+import sistemasanitario.entities.Paziente;
 
 /**
  *
  * @author marco
  */
+
+@WebServlet("/myservices/medico/pazienti")
 public class DashboardServlet extends HttpServlet {
 
+    private Dao<Paziente, Integer> pazienteDao;
+    private Dao<Medico, Integer> medicoDao;
+    private Gson gson = new Gson();
+    
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        
+        pazienteDao = (Dao<Paziente, Integer>)getServletContext().getAttribute("pazienteDao");
+        medicoDao = (Dao<Medico, Integer>)getServletContext().getAttribute("medicoDao");
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -31,7 +56,26 @@ public class DashboardServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        int id = Integer.parseInt(request.getParameter("idMedico"));
+        Medico medico = (Medico)request.getSession().getAttribute("medico");
+        
+        QueryBuilder<Paziente, Integer> queryBuilder = pazienteDao.queryBuilder();
+        PreparedQuery<Paziente> getPazienteByIdMedicoQuery;
+        
+        try {
+
+            getPazienteByIdMedicoQuery = queryBuilder.where().eq("idMedico", medico.getId()).prepare();
+            List<Paziente> pazienti = pazienteDao.query(getPazienteByIdMedicoQuery);
+            
+            String listaPazienti = this.gson.toJson(pazienti);
+            PrintWriter out = response.getWriter();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            out.print(listaPazienti);
+            out.flush();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- SistemaSanitario/src/main/webapp/js/pazientiMedico.js
+++ SistemaSanitario/src/main/webapp/js/pazientiMedico.js
@@ -1,14 +1,74 @@
 $(document).ready( function () {
     getPazienti();
     $('#myTable').DataTable({
-        "searching": false
-    } );
-    //$('.dataTables_length').addClass('bs-select');
+        "dom": 'lrtip'
+    });
 } );
 
 function getPazienti(){
+    
+    var p;
+    $.ajax({
+        url : "medico/pazienti",
+        dataType : 'json',
+        error : function() {
+            alert("Error Occured");
+        },
+        success : function(data) {
+        
+            var col = [];
+        for (var i = 0; i < data.length; i++) {
+            for (var key in data[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
 
-    var pazienti = [
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+        table.setAttribute('class', 'table');
+        table.setAttribute('id', 'myTable');
+        var header = table.createTHead();
+        header.setAttribute('class', 'thead-light');
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = header.insertRow(-1);                   // TABLE ROW.
+        
+        var arrayHeader = ["Cognome e nome","Ultima visita","Ultima ricetta"];
+        
+        for (var i = 0; i < arrayHeader.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = arrayHeader[i];
+            tr.appendChild(th);
+        }
+        
+        var body = table.createTBody();
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < data.length; i++) {
+
+            tr = body.insertRow(-1);
+
+            for (var j = 0; j < arrayHeader.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                if(j===0)
+                    tabCell.innerHTML = data[i][col[j]] + " " + data[i][col[j+1]];
+                else
+                    tabCell.innerHTML = data[i][col[j+1]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+
+        }
+    });
+
+    /*var pazienti = [
             {
                 "nome": "John",
                 "cognome": "Doe",
@@ -269,26 +329,14 @@ function getPazienti(){
         // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
         var divContainer = document.getElementById("showData");
         divContainer.innerHTML = "";
-        divContainer.appendChild(table);
+        divContainer.appendChild(table);*/
 }
 
-function myFunction() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }       
-    }
+function filterData() {
+    var table = $('#myTable').DataTable();
+    $('#myInput').on( 'keyup', function () {
+        table.search( this.value ).draw();
+    } );
 }
 
 
