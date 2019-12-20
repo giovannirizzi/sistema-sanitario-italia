d9d4d770183a3cc5dfbadc2226325c654b677c98
Marco <mark_mena97@hotmail.it>
2019-12-20 14:03:30 +0100

    Scheda pazienti per medico e prescrizioni di ogni paziente

--- SistemaSanitario/src/main/java/sistemasanitario/servlets/DashboardServlet.java
+++ SistemaSanitario/src/main/java/sistemasanitario/servlets/DashboardServlet.java
@@ -11,7 +11,12 @@ import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,19 +25,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import sistemasanitario.entities.Medico;
 import sistemasanitario.entities.Paziente;
+import sistemasanitario.entities.PrescrizioneEsame;
 
 /**
  *
  * @author marco
  */
 
-@WebServlet("/myservices/medico/pazienti")
+@WebServlet(name = "DashboardServlet", urlPatterns = {"/services/dashboard"})
 public class DashboardServlet extends HttpServlet {
 
     private Dao<Paziente, Integer> pazienteDao;
     private Dao<Medico, Integer> medicoDao;
+    private Dao<PrescrizioneEsame, Integer> prescrizioneEsameDao;
     private Gson gson = new Gson();
     
     @Override
@@ -41,6 +50,7 @@ public class DashboardServlet extends HttpServlet {
         
         pazienteDao = (Dao<Paziente, Integer>)getServletContext().getAttribute("pazienteDao");
         medicoDao = (Dao<Medico, Integer>)getServletContext().getAttribute("medicoDao");
+        prescrizioneEsameDao = (Dao<PrescrizioneEsame, Integer>)getServletContext().getAttribute("prescrizioneEsameDao");
     }
     
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -57,24 +67,70 @@ public class DashboardServlet extends HttpServlet {
             throws ServletException, IOException {
         
         Medico medico = (Medico)request.getSession().getAttribute("medico");
+        String id = request.getParameter("id");
         
-        QueryBuilder<Paziente, Integer> queryBuilder = pazienteDao.queryBuilder();
-        PreparedQuery<Paziente> getPazienteByIdMedicoQuery;
-        
-        try {
+        if(id == null){
+            QueryBuilder<Paziente, Integer> queryBuilder = pazienteDao.queryBuilder();
+            PreparedQuery<Paziente> getPazienteByIdMedicoQuery;
 
-            getPazienteByIdMedicoQuery = queryBuilder.where().eq("idMedico", medico.getId()).prepare();
-            List<Paziente> pazienti = pazienteDao.query(getPazienteByIdMedicoQuery);
+            try {
+                getPazienteByIdMedicoQuery = queryBuilder.where().eq("idMedico", medico.getId()).prepare();
+                List<Paziente> pazienti = pazienteDao.query(getPazienteByIdMedicoQuery);
+                
+                String listaPazienti = this.gson.toJson(pazienti);
+                PrintWriter out = response.getWriter();
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                out.print(listaPazienti);
+                out.flush();
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
             
-            String listaPazienti = this.gson.toJson(pazienti);
-            PrintWriter out = response.getWriter();
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            out.print(listaPazienti);
-            out.flush();
+        } else {
+            QueryBuilder<PrescrizioneEsame, Integer> queryBuilder2 = prescrizioneEsameDao.queryBuilder();
+            PreparedQuery<PrescrizioneEsame> getPrescrizioneEsameByIdPazienteQuery;
+            
+            try {
+                
+                JSONObject prescrizioni = new JSONObject();
+                
+                Connection connection = DriverManager.getConnection("jdbc:mysql://sistemasanitariodb.c16q8irjv9nd.us-east-2.rds.amazonaws.com/sistemasanitario", "admin", "Cinghiale123$");
+                // Creazione oggetto Statement
+                Statement statement = connection.createStatement();
 
-        } catch (SQLException ex) {
-            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+                // Esecuzione di un comando sul DB
+                ResultSet rs = statement.executeQuery("select PAZIENTE.nome,PAZIENTE.cognome,ESAME_PRESCRIVIBILE.nome as nomeEsame,ESAME_PRESCRIVIBILE.descrizione, PRESCRIZIONE_ESAME.data from PAZIENTE left join PRESCRIZIONE_ESAME on PAZIENTE.id = PRESCRIZIONE_ESAME.idPaziente inner join ESAME_PRESCRIVIBILE on PRESCRIZIONE_ESAME.idEsame = ESAME_PRESCRIVIBILE.id where PAZIENTE.id = " + id + ";");
+                
+                JSONArray array = new JSONArray();
+                JSONObject item = new JSONObject();
+                
+                // Elaborazione del risultato
+                while (rs.next()){
+                    item.put("name", rs.getString("nomeEsame"));
+                    item.put("description", rs.getString("descrizione"));
+                    item.put("date", rs.getString("data"));
+                    array.put(item);
+                }
+                
+                prescrizioni.put("exams", array);
+                
+                rs = statement.executeQuery("select nome,cognome from PAZIENTE where PAZIENTE.id = " + id + ";");
+                
+                while (rs.next()){
+                    prescrizioni.put("name", rs.getString("nome"));
+                    prescrizioni.put("surname", rs.getString("cognome"));
+                }
+                
+                PrintWriter out = response.getWriter();
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                out.print(prescrizioni);
+                out.flush();
+
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
--- SistemaSanitario/src/main/webapp/js/pazientiMedico.js
+++ SistemaSanitario/src/main/webapp/js/pazientiMedico.js
@@ -1,71 +1,149 @@
 $(document).ready( function () {
     getPazienti();
-    $('#myTable').DataTable({
-        "dom": 'lrtip'
-    });
+    /*$('#myTable').DataTable({
+        "dom": '<"top"i>rt<"bottom"lp><"clear">'
+    });*/
 } );
+
+$(document).on("click", "#myTable tr", function(e) {
+    
+    var id = this.id;
+    $.ajax({
+      url: "/SistemaSanitario/services/dashboard",
+      type: "get", //send it through get method
+      dataType : 'json',
+      data: {"id":id},
+      success: function(response) {
+            console.log(response);
+            $('#modalTitle').text("Scheda di "+ response.name + " " + response.surname);
+            
+            if(response.exams.length !== 0){
+                var col = [];
+                for (var i = 0; i < response.exams.length; i++) {
+                    for (var key in response.exams[i]) {
+                        if (col.indexOf(key) === -1) {
+                            col.push(key);
+                        }
+                    }
+                }
+
+                // CREATE DYNAMIC TABLE.
+                var table = document.createElement("table");
+                table.setAttribute('class', 'table');
+                table.setAttribute('id', 'mytable');
+                var header = table.createTHead();
+                header.setAttribute('class', 'thead-light');
+
+                // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+                var tr = header.insertRow(-1);                   // TABLE ROW.
+
+                var arrayHeader = ["Data","Esame","Descrizione"];
+
+                for (var i = 0; i < arrayHeader.length; i++) {
+                    var th = document.createElement("th");      // TABLE HEADER.
+                    th.innerHTML = arrayHeader[i];
+                    tr.appendChild(th);
+                }
+
+                var body = table.createTBody();
+
+                // ADD JSON DATA TO THE TABLE AS ROWS.
+                for (var i = 0; i < response.exams.length; i++) {
+
+                    tr = body.insertRow(-1);
+
+                    for (var j = 0; j < arrayHeader.length; j++) {
+                        var tabCell = tr.insertCell(-1);
+                        tabCell.innerHTML = response.exams[i][col[j]];
+                    }
+                }
+
+                // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+                var divContainer = document.getElementById("showPrescriptions");
+                divContainer.innerHTML = "";
+                divContainer.appendChild(table);
+            } else {
+                var divContainer = document.getElementById("showPrescriptions");
+                divContainer.innerHTML = "Nessuna prescrizione per questo paziente";
+            }
+          
+            
+          
+            $("#exampleModal").modal();
+      },
+      error: function(xhr, status, error) {
+         alert(xhr.responseText);
+      }
+    });
+});
 
 function getPazienti(){
     
     var p;
     $.ajax({
-        url : "medico/pazienti",
+        url : "/SistemaSanitario/services/dashboard",
         dataType : 'json',
-        error : function() {
-            alert("Error Occured");
+        error: function(xhr, status, error) {
+            alert(xhr.responseText);
         },
         success : function(data) {
         
             var col = [];
-        for (var i = 0; i < data.length; i++) {
-            for (var key in data[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
+            for (var i = 0; i < data.length; i++) {
+                for (var key in data[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
                 }
             }
-        }
+            console.log(col);
 
-        // CREATE DYNAMIC TABLE.
-        var table = document.createElement("table");
-        table.setAttribute('class', 'table');
-        table.setAttribute('id', 'myTable');
-        var header = table.createTHead();
-        header.setAttribute('class', 'thead-light');
+            // CREATE DYNAMIC TABLE.
+            var table = document.createElement("table");
+            table.setAttribute('class', 'table');
+            table.setAttribute('id', 'myTable');
+            var header = table.createTHead();
+            header.setAttribute('class', 'thead-light');
 
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+            // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
-        var tr = header.insertRow(-1);                   // TABLE ROW.
-        
-        var arrayHeader = ["Cognome e nome","Ultima visita","Ultima ricetta"];
-        
-        for (var i = 0; i < arrayHeader.length; i++) {
-            var th = document.createElement("th");      // TABLE HEADER.
-            th.innerHTML = arrayHeader[i];
-            tr.appendChild(th);
-        }
-        
-        var body = table.createTBody();
+            var tr = header.insertRow(-1);                   // TABLE ROW.
 
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < data.length; i++) {
+            var arrayHeader = ["Cognome e nome","Ultima visita","Ultima ricetta"];
 
-            tr = body.insertRow(-1);
-
-            for (var j = 0; j < arrayHeader.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                if(j===0)
-                    tabCell.innerHTML = data[i][col[j]] + " " + data[i][col[j+1]];
-                else
-                    tabCell.innerHTML = data[i][col[j+1]];
+            for (var i = 0; i < arrayHeader.length; i++) {
+                var th = document.createElement("th");      // TABLE HEADER.
+                th.innerHTML = arrayHeader[i];
+                tr.appendChild(th);
             }
-        }
 
-        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        var divContainer = document.getElementById("showData");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
+            var body = table.createTBody();
 
-        }
+            // ADD JSON DATA TO THE TABLE AS ROWS.
+            for (var i = 0; i < data.length; i++) {
+
+                tr = body.insertRow(-1);
+                tr.setAttribute('id',""+data[i]["id"]+"");
+
+                for (var j = 0; j < arrayHeader.length; j++) {
+                    var tabCell = tr.insertCell(-1);
+                    if(j===0)
+                        tabCell.innerHTML = data[i][col[j+2]] + " " + data[i][col[j+3]];
+                    else
+                        tabCell.innerHTML = data[i][col[j+3]];
+                    
+                }
+            }
+
+            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+            var divContainer = document.getElementById("showData");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
+            $("#myTable").dataTable({
+                "dom": '<"top"i>rt<"bottom"lp><"clear">'
+            });
+        }  
     });
 
     /*var pazienti = [
