07160343f111f62ff65c8ac6d981e8c1bc73d95d
Omar Battan <omarbattan@gmail.com>
2019-12-20 12:24:54 +0100

    Gestione codici di ritorno + alert. Da sistemare alcuni bug

--- SistemaSanitario/src/main/webapp/js/changePassword.js
+++ SistemaSanitario/src/main/webapp/js/changePassword.js
@@ -1,20 +1,42 @@
 $("document").ready(function() {
     // Add an event that triggers when the submit
     // button is pressed.
-    $("#myForm").submit(function() {
+    $("#submitPassword").click(function() {
         var oldpassword = $("#oldpassword").val();
         var newpassword = $("#newpassword").val();
-        alert("hai fatto il submit gay");
-        alert(oldpassword);
-        alert(newpassword);
-
         // Ajax POST request, similar to the GET request.
         $.post('../services/changepassword',{"oldpassword": oldpassword, "newpassword": newpassword},
             function() { // on success
-                alert("Insertion successful!");
+                $("#error").html("<div class='alert alert-success' role='alert'> <b>Password cambiata con successo.</b> </div>");
+                $("#error").removeClass("d-none").addClass("d-block");
+                $("#modalPassword").reload();
             })
-            .fail(function() { //on failure
-                alert("Insertion failed.");
+            .fail(function(error) { //on failure
+                if(error.status===400){
+                    $("#error").html("<div class='alert alert-danger' role='alert'> <b>Impossibile cambiare la password.</b> Errore. </div>");
+                    $("#error").removeClass("d-bloc").addClass("d-none");
+                    $("#modalPassword").reload();
+                }
+                if(error.status===490){
+                    $("#error").html("<div class='alert alert-danger' role='alert'> <b>Impossibile cambiare la password.</b> La nuova password non rispetta le policy di sicurezza. </div>");
+                    $("#error").removeClass("d-none").addClass("d-block");
+                    $("#modalPassword").reload();
+                }
+                if(error.status===491){
+                    $("#error").html("<div class='alert alert-danger' role='alert'> <b>Impossibile cambiare la password.</b> La password corrente non coincide. </div>");
+                    $("#error").removeClass("d-none").addClass("d-block");
+                    $("#modalPassword").reload();
+                }
+                if(error.status===500){
+                    $("#error").html("<div class='alert alert-danger' role='alert'> <b>Impossibile cambiare la password.</b> Riprova piu' tardi. </div>");
+                    $("#error").removeClass("d-none").addClass("d-block");
+                    $("#modalPassword").reload();
+                }
+            })
+            .done(function(msg) { // ok
+                $("#error").html("<div class='alert alert-success' role='alert'> <b>Password cambiata con successo.</b> </div>");
+                $("#error").removeClass("d-none").addClass("d-block");
+                $("#modalPassword").reload();
             });
     }); 
 
